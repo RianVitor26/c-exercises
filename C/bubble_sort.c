d61d1b32d13b297ab91bf24bc8a985b45c894875
RianVitor26 <rianvitorlhe@gmail.com>
2022-06-21 20:17:30 -0300

    algoritmo de ordenação

---
+++ C/bubble_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void insertionSortRecursive(int arr[], int n) {
+  if (n <= 1) {
+    return;
+  }
+  
+  insertionSortRecursive(arr, n - 1);
+
+  int last = arr[n - 1];
+  int j = n - 2;
+
+  while (j >= 0 && arr[j] > last) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = last;
+}
+
+void printArray(int arr[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+  printf("%d ", arr[i]);
+  printf("\n");
+}
+
+int main() {
+  int arr[4] = {9, 6, 5, 3};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  insertionSortRecursive(arr, n);
+  printArray(arr, n);
+
+  return 0;
+}
