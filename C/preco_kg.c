1c33703fd1df8e1811271c917304a618dffb3f5c
RianVitor26 <rianvitorlhe@gmail.com>
2022-06-27 20:43:54 -0300

    Cálculo do kg de um produto com base no preço total e peso total

--- C/preco_kg.c
+++ C/preco_kg.c
@@ -1,44 +1,36 @@
 #include <stdio.h>
 
-struct Produto{
-	char descricao[30];
-	float peso_total;
-	float preco_total;
-	float preco_kg;
-};
-
-typedef struct {
-	char descricao[30];	
-	float peso_total;
-	float preco_total;
-	float preco_kg;
-} Produto;
-
-Produto produto;
-
-float calcula_preco_kg(float peso, float preco){
-
-	float preco_kg = preco / peso;
-	
-	return preco_kg;
+float calcular_media(float nota1, float nota2) {
+  float media = (nota1 + nota2) / 2;
+  return media;
 }
 
-int main(){
+float calcular_mediaTurma(float notaTurma) {
+  float mediaTurma = notaTurma / 5;
+  return mediaTurma;
+}
 
-	Produto produto;
+typedef struct {
+  char nome_aluno[30];
+  float nota_1;
+  float nota_2;
+  float media;
+} Aluno;
 
-	printf("Insira a descricao do produto: \n");
-	scanf("%s", &produto.descricao[30]);
-	printf("Insira o Peso total do produto: \n");
-	scanf("%f", &produto.peso_total);
-	printf("Insira o Preco Total do produto: \n");
-	scanf("%f", &produto.preco_total);
-
-
-	float preco_kg = calcula_preco_kg(produto.peso_total, produto.preco_total);
-
-	printf("Descricao do Produto: %s \n", produto.descricao);
-	printf("Peso Total do Produto: %f \n", produto.peso_total);
-	printf("Preco Total do Produto: %f \n", produto.preco_total);	
-	printf("Peso por KG Total: %f \n", preco_kg);
-	}
+int main() {
+  Aluno alunos[5];
+  float media, mediaTurma, notaTurma;
+  for (int i = 0; i < sizeof(alunos); i++) {
+    printf("Insira o nome do aluno %d: ", i);
+    scanf("%s", &alunos[i].nome_aluno[30]);
+    printf("Insira a nota 1 do aluno: ");
+    scanf("%f", &alunos[i].nota_1);
+    printf("Insira a nota 2 do aluno: ");
+    scanf("%f", &alunos[i].nota_2);
+    media = calcular_media(alunos[i].nota_1, alunos[i].nota_2);
+    printf("A media do aluno %i é: %.2f \n", i, media);
+    notaTurma = notaTurma + media;
+  }
+  mediaTurma = calcular_mediaTurma(notaTurma);
+  printf("A média da turma é: %.2f", mediaTurma);
+}
