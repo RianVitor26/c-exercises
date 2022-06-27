e84f67d958b078eea242be6fa4d86e2db42f930b
RianVitor26 <rianvitorlhe@gmail.com>
2022-06-27 20:38:40 -0300

    finalizando o algoritmo de preço por kg de um produto

--- C/preco_kg.c
+++ C/preco_kg.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-float preco_kg(float preco_total, float peso_total){
-  float preco_kg = preco_total / peso_total;
-  return preco_kg;
-}
+struct Produto{
+	char descricao[30];
+	float peso_total;
+	float preco_total;
+	float preco_kg;
+};
 
 typedef struct {
-char descricao[30];
-float peso_total;
-float preco_total;
-float preco_kg;
+	char descricao[30];	
+	float peso_total;
+	float preco_total;
+	float preco_kg;
 } Produto;
 
-int main(void){
-  Produto carne;
-  
-  printf("Iforme o valor dessa carne: ");
-  scanf("%f", &carne.preco_total);
-  printf("Iforme o peso dessa carne: ");
-  scanf("%f", &carne.peso_total);
-  
-  carne.preco_kg = preco_kg(carne.preco_total,     
-  carne.peso_total);
-  
-  printf("%2.f reais o preço do kg", carne.preco_kg);
-  return 0;
+Produto produto;
+
+float calcula_preco_kg(float peso, float preco){
+
+	float preco_kg = preco / peso;
+	
+	return preco_kg;
 }
+
+int main(){
+
+	Produto produto;
+
+	printf("Insira a descricao do produto: \n");
+	scanf("%s", &produto.descricao[30]);
+	printf("Insira o Peso total do produto: \n");
+	scanf("%f", &produto.peso_total);
+	printf("Insira o Preco Total do produto: \n");
+	scanf("%f", &produto.preco_total);
+
+
+	float preco_kg = calcula_preco_kg(produto.peso_total, produto.preco_total);
+
+	printf("Descricao do Produto: %s \n", produto.descricao);
+	printf("Peso Total do Produto: %f \n", produto.peso_total);
+	printf("Preco Total do Produto: %f \n", produto.preco_total);	
+	printf("Peso por KG Total: %f \n", preco_kg);
+	}
